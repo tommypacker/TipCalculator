969d53c4680e80a2a2c7f84572ce922e866201b4
tommypacker <tommypacker@gmail.com>
2015-07-24 14:13:11 -0700

    Initial SplitBill Option added

--- app/src/main/java/com/tommypacker/tipcalculator/Calculator.java
+++ app/src/main/java/com/tommypacker/tipcalculator/Calculator.java
@@ -19,6 +19,16 @@ public abstract class Calculator {
         return mealPrice/numberOfPeople;
     }
 
+    public static double tipsPerMultiplePeople(double mealPrice, int tipRate, int numberOfPeople){
+        double ratePercentage = (double) tipRate * .01;
+        double totalTips = ratePercentage * mealPrice;
+
+        totalTips = Math.round(totalTips*100);
+        totalTips = totalTips/100;
+
+        return totalTips/numberOfPeople;
+    }
+
     public static double taxPrice(double mealPrice, double taxRate){
         double taxPercentage = taxRate/100.0;
 
--- app/src/main/java/com/tommypacker/tipcalculator/InputFragment.java
+++ app/src/main/java/com/tommypacker/tipcalculator/InputFragment.java
@@ -2,6 +2,7 @@ package com.tommypacker.tipcalculator;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +16,10 @@ import java.util.zip.Inflater;
 /**
  * Created by tommypacker on 7/20/15.
  */
-public class InputFragment extends Fragment {
+public class InputFragment extends Fragment implements SplitBillDialog.onSplitSubmit{
 
     private onSubmit displayResult;
-    private EditText price, rate, numPeople;
+    private EditText price, rate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -26,8 +27,16 @@ public class InputFragment extends Fragment {
         price = (EditText) view.findViewById(R.id.MealPrice);
         rate = (EditText) view.findViewById(R.id.TipRate);
 
-        Button b = (Button) view.findViewById(R.id.SubmitButton);
-        b.setOnClickListener(new View.OnClickListener() {
+        Button split = (Button) view.findViewById(R.id.splitBill);
+        split.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
+        Button submit = (Button) view.findViewById(R.id.SubmitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculateValues();
@@ -61,6 +70,30 @@ public class InputFragment extends Fragment {
 
         double resultFromCalculator = Calculator.tipsPerOnePerson(mealPrice, tipRate);
         displayResult.onUserSubmit(mealPrice, resultFromCalculator);
+    }
+
+    public void calculateSplitValues(int numPeople){
+        double mealPrice = Double.parseDouble(price.getText().toString());
+        int tipRate = Integer.parseInt(rate.getText().toString());
+
+        if(mealPrice < 0 || tipRate < 0 || numPeople < 0){
+            Toast.makeText(getActivity(), "Please enter a valid number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double mealPricePerPerson = Calculator.mealPricePerPerson(mealPrice, numPeople);
+        double tipsPerPerson = Calculator.tipsPerMultiplePeople(mealPrice, tipRate, numPeople);
+        displayResult.onUserSubmit(mealPricePerPerson, tipsPerPerson);
+    }
+
+    public void openDialog(){
+        FragmentManager manager = getFragmentManager();
+        SplitBillDialog billSplitter = new SplitBillDialog();
+        billSplitter.show(manager, "Divide Bill");
+    }
+
+    public void transferNumber(int numPeople){
+        calculateSplitValues(numPeople);
     }
 
 }
